b30a6182545f355bd84d2d367d2944a56dfc23a4
Jeremiah Olisa <jeremiaholisa453@gmqil.com>
2022-04-16 17:00:18 +0100

    index all methods and class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import _apiFeatures from './api/api-features';
+import _appError from './mongoose/app-error';
+import _catchAsync from './mongoose/catch-async';
+import _connect from './mongoose/db-connect';
+import _filterObj from './mongoose/filterObject';
+import _globalErrorHandler from './mongoose/global-error-handler';
+import _middleware from './api/middleware';
+import _mongoFactory from './mongoose/factory';
+import _response from './mongoose/response-handler';
+
+export const AppError = _appError;
+export const catchAsync = _catchAsync
+export const dbConnect = _connect
+export const mongoFactory = _mongoFactory
+export const FilterObj = _filterObj
+export const globalErrorHandler = _globalErrorHandler
+export const responseHandler = _response
+export const APIFeatures = _apiFeatures
+export const middleware = _middleware
+
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
+// export const AppError = _appError
